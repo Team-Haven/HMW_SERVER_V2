0ec5122c62526a5caeb6d0d4088a8bd1e83e294c
soohyang136 <jsh515228@gmail.com>
2023-07-05 14:55:28 +0900

    init

---
+++ src/main/java/haven/hmw/HmwApplication.java
@@ -0,0 +1,13 @@
+package haven.hmw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/haven/hmw/HmwApplicationTests.java
@@ -0,0 +1,13 @@
+package haven.hmw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
